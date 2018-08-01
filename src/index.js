6cf50a9cecfbf34304b20cfaf016494f5d8e4907
carlchan <carlchan.dev@gmail.com>
2018-08-02 00:08:22 +0800

    added TaskList, Column, Task components

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import initialData from './initial-data'
+import Column from "./column";
+import '@atlaskit/css-reset';
+
+class App extends Component {
+    state = initialData;
+    render() {
+        return this.state.columnOrder.map(columnId => {
+            console.log(columnId);
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+            return <Column key={column.id} column={column} tasks={tasks} />
+        });
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
