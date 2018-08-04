55942d75a819605f6248484349cbb876113e105d
Carl Chan <carlchan.dev@gmail.com>
2018-08-04 18:23:02 +0800

    updated coupon data

--- src/initial-data.js
+++ src/initial-data.js
@@ -1,16 +1,16 @@
 const initialData = {
   tasks: {
-    'task-1': { id: 'task-1', content: 'Dino God' },
-    'task-2': { id: 'task-2', content: 'Derrick God' },
-    'task-3': { id: 'task-3', content: 'Andy God' },
-    'task-4': { id: 'task-4', content: 'Nic God' },
-    'task-5': { id: 'task-5', content: 'Dragon God' },
-    'task-6': { id: 'task-5', content: 'Clement God' },
+    'task-1': { id: 'task-1', content: '行山券' },
+    'task-2': { id: 'task-2', content: '運動券' },
+    'task-3': { id: 'task-3', content: '婆婆券' },
+    'task-4': { id: 'task-4', content: '飲茶券' },
+    'task-5': { id: 'task-5', content: '咖啡券' },
+    'task-6': { id: 'task-5', content: '睇戲券' },
   },
   columns: {
     'column-1': {
       id: 'column-1',
-      title: 'Gods of the World',
+      title: '今日有咩券兌換?!',
       taskIds: [
         'task-1',
         'task-2',
